d32c096d5a1ee9df6c044830986f94bab87f596e
KaterynaNat <kate.naturkach@gmail.com>
2024-11-06 02:58:27 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,52 +1,40 @@
-'use strict';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.form');
-const input = document.querySelector('.delay');
-let delay = null;
+const formEl = document.querySelector('.form');
 
-input.addEventListener('input', event => {
-  delay = event.target.value;
-});
-
-form.addEventListener('submit', event => {
+formEl.addEventListener('submit', event => {
   event.preventDefault();
 
-  let checked = document.querySelector('input[name="state"]:checked');
+  const delay = event.currentTarget.elements.delay.value;
+  const state = event.currentTarget.elements.state.value;
 
+  createPromise(delay, state)
+    .then(() => {
+      iziToast.success({
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(() => {
+      iziToast.error({
+        position: 'topRight',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+
+  formEl.reset();
+});
+
+function createPromise(delay, state) {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (checked.value === 'fulfilled') {
-        resolve(delay);
+      if (state === 'fulfilled') {
+        resolve();
       } else {
-        reject(delay);
+        reject();
       }
     }, delay);
   });
-  promise.then(value => {
-    iziToast.error({
-      message: `✅ Fulfilled promise in ${value}ms`,
-      position: 'topCenter',
-      color: 'green',
-      messageColor: 'black',
-      close: false,
-      timeout: 4000,
-      progressBar: false,
-      icon: false,
-    });
-  });
-  promise.catch(err => {
-    iziToast.error({
-      message: `❌ Rejected promise in ${err}ms`,
-      position: 'topCenter',
-      color: 'red',
-      messageColor: 'black',
-      close: false,
-      timeout: 4000,
-      progressBar: false,
-      icon: false,
-    });
-  });
-  form.reset();
-});
+  return promise;
+}
